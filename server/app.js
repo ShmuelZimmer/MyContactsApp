4562f33ed83cbcfc27b36cf043f367a704ed5e15
ShmuelZimmer <shmuelszimmer@gmail.com>
2024-12-11 13:12:50 +0200

    fixed path to serve index.html again

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ mongoose.connect(
 if(process.env.NODE_ENV == "production"){
     app.use(express.static(path.join(__dirname, "..", "/client/dist")))
     app.get("*", (req, res) =>{
-        res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+        res.sendFile(path.resolve(__dirname,"..", "/client/dist/index.html"))
     })
 }
 

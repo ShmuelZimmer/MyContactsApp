1afc1c7e22f854b67bf246891c52a65d4b0b8402
ShmuelZimmer <shmuelszimmer@gmail.com>
2024-12-11 13:40:38 +0200

    changed error on reg

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
       nav('/')
     
     }catch(err){
-      console.log(err.response.data.error);
+      console.log(err.response);
       alert("Registration Error")
     }
 
